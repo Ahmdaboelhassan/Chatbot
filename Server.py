00dcd554091d5aaf3b06e464d63796fdbb38192e
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2024-02-16 05:53:23 +0200

    intial Commit

---
+++ Server.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+from chatbot_response import chatbot_response
+# from waitress import serve
+
+# Model Names
+General = "General"
+Depression = "Depression"
+Anxiety = "Anxiety"
+Stress = "Stress"
+SleepDisorder = "SleepDisorder"
+Phobia = "Phobia"
+PersonaltyDisorder = "PersonaltyDisorder"
+
+# Array of Names
+app_models = [Depression, Anxiety, Stress, SleepDisorder, Phobia, PersonaltyDisorder]
+
+# Key Strings
+ClearKeyMsgs = ["forget-memory", "clear-memory", "forget-model", "clear-model"]
+CookieKey = "SelectedModel"
+
+app = Flask(__name__)
+cors = CORS(app)
+
+# helping method
+def server_response(msg, model_name=General):
+    result = chatbot_response(msg, model_name)
+    response = make_response(jsonify({'answer': result}))
+    response.set_cookie(CookieKey, model_name, max_age=1800)
+    return response
+
+
+# http://localhost:5000/ChatBot?msg=""
+@app.route('/ChatBot/', methods=['GET'])
+def chatbot():
+    model_from_cookie = request.cookies.get(CookieKey)
+    msg = request.args.get('msg')
+
+    if msg in ClearKeyMsgs:
+        response = make_response(jsonify({'answer': "Memory Cleared Successfully"}))
+        response.set_cookie(CookieKey, General, max_age=180)
+        return response
+
+    if model_from_cookie is None or model_from_cookie not in app_models:
+        result = chatbot_response(msg)
+        if result in app_models:
+            return server_response(msg, result)
+        return server_response(msg)
+
+    return server_response(msg, model_from_cookie)
+
+
+# http://localhost:5000/ChatBot/Disease?name=""
+@app.route('/ChatBot/Disease', methods=['GET'])
+def chatbot_disease():
+    disease_name = request.args.get('name')
+
+    if (disease_name is not None) and (disease_name in app_models):
+        res = make_response(jsonify({'answer': f"Tell me what you want to Know About {disease_name}"}))
+        res.set_cookie(CookieKey, disease_name)
+        return res
+
+    return jsonify({'answer': f"I Don't have knowledge About {disease_name} yet"})
+
+
+if __name__ == '__main__':
+    # print()
+    # print("##"*50)
+    # print("App Is Running  Successfully On These APIs")
+    # print("==> http://localhost:5000/ChatBot?msg=")
+    # print("==> http://localhost:5000/ChatBot/Disease?name=")
+    # print("##"*50)
+    # serve(app, host="0.0.0.0", port=5000)
+    app.run()
---
+++ chatbot_response.py
@@ -0,0 +1,83 @@
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from pickle import load as load_pickle
+from json import loads as load_json
+from random import choice as random_choice
+from numpy import array as np_array
+from keras.models import load_model
+from os import path
+
+
+
+def prepare_sentence(current_sentence, current_words):
+    lemmatizer = WordNetLemmatizer()
+
+    # tokenize the pattern
+    sentence_words = word_tokenize(current_sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(current_words)
+    for sentence_word in sentence_words:
+        for i, word in enumerate(current_words):
+            if word == sentence_word:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+
+    return np_array(bag)
+
+
+def predict_class(current_sentence, current_model, current_words, current_classes):
+    # filter out predictions below a threshold
+    sentence = prepare_sentence(current_sentence, current_words)
+
+    res = current_model.predict(np_array([sentence]))[0]
+
+    ERROR_THRESHOLD = 0.1
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": current_classes[r[0]], "probability": str(r[1])})
+        print({"intent": current_classes[r[0]], "probability": str(r[1])})
+
+    return return_list
+
+
+def get_response(ints, dataset):
+    result = "I don't Qualfied To Understand this yet"
+    if len(ints) != 0:
+        tag = ints[0]['intent']
+        list_of_intents = dataset['intents']
+        for i in list_of_intents:
+            if (i['tag'] == tag):
+                result = random_choice(i['responses'])
+                break
+    return result
+
+
+# Paths
+cwd = path.dirname(path.abspath(__file__))
+def get_model_paths(model_name):
+    model = cwd + f"\\{model_name}\\chatbot_model.h5"
+    words = cwd + f"\\{model_name}\\words.pkl"
+    classes = cwd + f"\\{model_name}\\classes.pkl"
+    dataset = cwd + f"\\{model_name}\\{model_name}.json"
+    return model, words, classes, dataset
+
+
+def chatbot_response(msg, model_from_cookie="General"):
+    # Get Paths
+    model_path, words_path, classes_path, dataset_path = get_model_paths(model_from_cookie)
+
+    # load files
+    model = load_model(model_path)
+    classes = load_pickle(open(classes_path, "rb"))
+    words = load_pickle(open(words_path, 'rb'))
+    dataset = load_json(open(dataset_path, encoding='utf-8').read())
+
+    ints = predict_class(msg, model, words, classes)
+    res = get_response(ints, dataset)
+    return res
